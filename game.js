19ce6d1587829cf9e6fc7c79f2880cbfc66589d9
mwinterfeld <michael.winterfeld@colorado.edu>
2016-06-08 21:35:38 -0600

    Workin

--- game.js
+++ game.js
@@ -1,50 +1,51 @@
-function setupSocket(){
-    window.socket = io.connect('/');
-    window.socket.emit("returninguser", {id: $.cookie("UUID")});
+function Card(x, y, stage){
+    this.rect = new createjs.Shape(new createjs.Graphics().f("#eeeeee").s("rbga(20,20,20,1)").rr(x, y, Card.width, Card.height, 3));
+    this.text = new createjs.Text(Card.available[Math.floor((Math.random() * Card.available.length))], "20px Serif", "#212121"); 
+    this.text.x = x + Card.width / 2 - this.text.getBounds().width / 2;
+    this.text.y = y + Card.height / 3;
+    stage.addChild(this.rect);
+    stage.addChild(this.text);
 }
 
-function joinGame(){
-    window.socket.on("invalidgame", function(){
-        window.location = "/";
-    });
-    window.socket.emit("joingame", {"userid": $.cookie("UUID"), "gameid": document.location.pathname.replace("/game/", ""), "name": $.cookie("name")});
-}
+Card.width = 150;
+Card.height = 200;
+
+Card.available = [
+    "Boat", "Dog", "Pickle", "Foot", "Needle", "Washington", "France", "Pizza", "Water",
+    "Kitten", "Dirt", "Hill", "Sky", "Train", "Light", "Poster", "Paper", "Pyramid", "Ice",
+    "Volcano", "Canary", "House", "Spaghetti", "Ocean", "Banana", "Monarch", "Spindle",
+    "Phone", "Clock", "Jacuzzi", "Hair", "Lamp", "Caterpillar", "Avocado", "Stick",
+    "Meringue", "Finger", "Bottle", "Granite", "Dust", "Smoke", "Fan", "Air", "Baby",
+    "Windwill", "Hacksaw", "Box", "Washing\nMachine", "Island", "Spatula", "Bowl", "Canada",
+    "Turkey", "Painting", "Room", "Snowman", "Cup", "Tea", "Ballsack", "Sake", "Kitty",
+    "Rope", "Table", "Cell", "Tomato", "Italian", "Doorbell", "Dinosaur", "Planet", "Rock",
+    "Amoeba", "Jelly", "Beans", "Fart", "Cake", "Auditorium", "Penis", "Whale", "Forest",
+    "Island", "Strip Club", "Gabriel\nIglesias", "San Francisco", "Peter\nDinklage",
+    "Key", "Television", "Garbage", "Hurricane", "Sharknado", "Cockroach", "Bedbug",
+    "Bed", "Pillow", "Fire", "Campfire", "Log", "Mansion", "Celebrity", "Iceberg", "Ferret",
+    "Fairy", "Magic", "Totoro", "The\nTerminator", "Arnold\nSchwarzenegger", "Book", "Idea",
+    "Blood", "Chromosome", "Heart", "Liver", "Toe", "Nail", "Cloud", "Space", "Black Hole",
+    "Spaceship", "USS\nEnterprise", "Nebula", "Asteroid", "Desktop", "Liquid", "Phone",
+    "Paint", "Can", "Pot", "Powder", "Snowflake", "Rain", "Mountain", "River", "Mitten",
+    "Couch", "Water Bear", "Canyon", "The\nInternet", "Your Momma", "Your Papa", "Beetle",
+    "Greatness", "Shower", "Shower Caddy", "Lots Of\nBreasts", "Raw Meat", "Bullet", "Titan",
+    "The Death\nStar", "Electron", "Quark", "Quantum\nField", "Leaves", "Number", "Wrench",
+    "Word", "Alphabet", "Infinity", "Lingerie", "Shade"
+];
+
+Card.inplay = [];
 
 function runGame(){
-    $("html").keyup(function(e){
-        $("ul").append("<li>" + $.cookie("name") + ": " + e.keyCode + "</li>");
-        window.socket.emit('keyup', {"id": $.cookie("UUID"), "msg": e.keyCode});
-    });
-
-    window.socket.on("keyup", function(data){
-        $("ul").append("<li>" + data + "</li>");
-    });
+    var CANVAS_WIDTH = $(document).innerWidth() * 0.9;
+    var CANVAS_HEIGHT = $(document).innerHeight() * 0.9;
+    $("#canvas").attr("height", CANVAS_HEIGHT);
+    $("#canvas").attr("width", CANVAS_WIDTH);
+    var stage = new createjs.Stage("canvas");
+    var c1 = new Card(50, 50, stage);
+    var c2 = new Card(160, 60, stage);
+    var c3 = new Card(270, 70, stage);
+    var c4 = new Card(380, 80, stage);
+    stage.update();
 }
 
-$(document).ready(function(){
-    setupSocket();
-    joinGame();
-    runGame();
-
-    // var CANVAS_WIDTH = $(document).innerWidth() * 0.9;
-    // var CANVAS_HEIGHT = $(document).innerHeight() * 0.9;
-    // $("#canvas").attr("height", CANVAS_HEIGHT);
-    // $("#canvas").attr("width", CANVAS_WIDTH);
-
-    // var stage = new createjs.Stage("canvas");
-
-    // var circle = new createjs.Shape();
-    // circle.graphics.beginFill("DeepSkyBlue").drawCircle(0, 0, 50);
-    // circle.x = 100;
-    // circle.y = 100;
-    // stage.addChild(circle);
-
-    // createjs.Ticker.addEventListener("tick", tick);
-    // function tick(event) {
-    //     circle.x += event.delta/1000*100;
-    //     if(circle.x > CANVAS_WIDTH + 50){
-    //         circle.x = 0;
-    //     }
-    //     stage.update();
-    // }
-});
+runGame();
--- index.js
+++ index.js
@@ -9,7 +9,8 @@ game = function(name, pid, pname){
     var obj = {
         name: name,
         players: [pid],
-        player_names: {}
+        player_names: {},
+        status: "lobby"
     }
     obj.player_names[pid] = pname;
     return obj;
@@ -20,8 +21,13 @@ app.get('/', function(req, res){
 });
 
 
-app.get( '/game/*', function(req, res, next){
-    res.sendFile(__dirname + "/game.html");
+app.get('/game/*', function(req, res, next){
+    if(req.params[0].indexOf(".js") !== -1){
+        res.sendFile(__dirname + "/" + req.params[0])
+    }
+    else {
+        res.sendFile(__dirname + "/game_lobby.html");
+    }
 });
 
 app.get( '/*' , function( req, res, next ) {
@@ -29,7 +35,7 @@ app.get( '/*' , function( req, res, next ) {
     var file = req.params[0]; 
 
     //For debugging, we can track what files are requested.
-    // console.log('\t ::Express:: file requested : ' + file);
+    console.log('\t ::Express:: file requested : ' + file);
 
     //Send the requesting client the file.
     res.sendFile( __dirname + '/' + file );
@@ -59,38 +65,50 @@ io.on('connection', function(client){
 
     client.on("joingame", function(data){
         if(!(data.gameid in games)){
-            console.log("\tInvalid game id: " + data.gameid)
+            console.log("\t Invalid game id: " + data.gameid)
             client.emit("invalidgame");
             return;
         }
         client.gameid = data.gameid;
         client.join(client.gameid);
-        console.log("\t" + data.userid + " joined " + games[client.gameid].name);
+        console.log("\t " + data.userid + " joined " + games[client.gameid].name);
         if(games[client.gameid].players.indexOf(data.userid) === -1){
             games[client.gameid].players.push(data.userid);
             games[client.gameid].player_names[data.userid] = data.name;
         }
-        console.log(games[data.gameid].players);
+        client.emit("initialplayers", games[client.gameid].player_names);
+        var newplayer = {};
+        newplayer["id"] = client.userid;
+        newplayer["name"] = games[client.gameid].player_names[client.userid]
+        client.broadcast.to(client.gameid).emit("newplayer", newplayer);
     });
 
-    client.on("keyup", function(data){
-        console.log(data.id);
-        console.log(games[client.gameid].player_names)
-        console.log(games[client.gameid].player_names[data.id])
-        client.broadcast.to(client.gameid).emit("keyup", games[client.gameid].player_names[data.id] + ": " + data.msg);
+    client.on("lobbymessage", function(data){
+        client.broadcast.to(client.gameid).emit("lobbymessage", games[client.gameid].player_names[client.userid] + ": " + data);
     });
+
+    // client.on("keyup", function(data){
+    //     console.log(data.id);
+    //     console.log(games[client.gameid].player_names)
+    //     console.log(games[client.gameid].player_names[data.id])
+    //     client.broadcast.to(client.gameid).emit("keyup", games[client.gameid].player_names[data.id] + ": " + data.msg);
+    // });
     
-    //When this client disconnects
     client.on('disconnect', function (data) {
-        client.leave(client.gameid);
         if(client.gameid in games){
+            client.broadcast.to(client.gameid).emit("leavegame", {"id": client.userid, "name": games[client.gameid].player_names[client.userid]})
+            delete games[client.gameid].player_names[client.userid];
             games[client.gameid].players.splice(client.gameid);
-            delete games[client.gameid].player_names[client.gameid];            
+            if(games[client.gameid].players.length === 0){
+                console.log("\t game " + client.gameid + " empty, deleting it");
+                delete games[client.gameid];
+            }
         }
+        client.leave(client.gameid);
         console.log('\t ::socket.io:: client disconnected ' + client.userid );
-    }); //client.on disconnect
+    });
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+http.listen(55001, function(){
+  console.log('listening on *:55001');
 });
